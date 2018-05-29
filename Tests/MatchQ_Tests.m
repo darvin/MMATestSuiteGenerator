83ec22fa62b82dc6aff48a95a519c87ec93d0125
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2018-05-29 20:23:13 +0000

    Deploy darvin/MMATestSuiteGenerator to github.com/darvin/MMATestSuiteGenerator.git:gh-pages

--- Tests/MatchQ_Tests.m
+++
@@ -1,8 +0,0 @@
-ESimpleExamples[EComment["Test if an expression is an integer:"], 
- ESameTest[MatchQ[12345, _Integer], True], 
- EComment["Test if an expression is a sum of two or more terms:"], 
- ESameTest[MatchQ[(x - 1)*(1 + 2*x + 3*x^2), _ + __], False], 
- ESameTest[MatchQ[Expand[x*(1 + 2*x + 3*x^2)], _ + __], True], 
- EComment["Test if an expression is explicitly zero:"], 
- ESameTest[MatchQ[1 + 1/GoldenRatio - GoldenRatio, 0], False], 
- ESameTest[MatchQ[Simplify[1 + 1/GoldenRatio - GoldenRatio], 0], True]]
