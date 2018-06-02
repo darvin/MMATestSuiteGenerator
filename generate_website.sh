54bd16c2ca2c8aedee3b217505a48d9a9361fe30
Sergey Klimov <sergey.v.klimov@gmail.com>
2018-06-02 00:22:05 -0700

    website generator

--- generate_website.sh
+++ generate_website.sh
@@ -1,3 +1,41 @@
+#!/bin/bash
+set -e
+# marge --reportDir ./ --showSkipped false --showHooks never --showPending false --showPassed false 
+
+marge=`pwd`/node_modules/.bin/marge
+concatTaps=`pwd`/node_modules/tap-mochawesome-reporter/concatTaps.sh
+tapMochawesome=`pwd`/node_modules/.bin/tap-mochawesome-reporter
+MAX_MEM=8192
+processFile () {
+	FILE=$1
+	BASENAME=`basename $FILE`
+	JSON="$BASENAME.json"
+	echo "   > $FILE"
+	cat $FILE |  $tapMochawesome > $JSON
+	$marge -t $BASENAME -p $BASENAME  --reportDir ./ --showSkipped false --showHooks never --showPending false --showPassed false $JSON
+
+}
+
+processDirectory () {
+	DIR=$1
+	SYSTEM_NAME=`basename $DIR`
+	echo "> $SYSTEM_NAME"
+	pushd $DIR
+	# $concatTaps *.tap | node --max-old-space-size=$MAX_MEM $tapMochawesome > ALL_TESTS.json
+	# node --max-old-space-size=$MAX_MEM $marge -t $SYSTEM_NAME -p $SYSTEM_NAME  --reportDir ./ --showSkipped false --showHooks never --showPending false --showPassed false ALL_TESTS.json
+
+	for f in *.tap ; do
+	    processFile $f
+	done
 
 
-marge --reportDir ./ --showSkipped false --showHooks never --showPending false --showPassed false 
+	popd
+}
+
+for d in output/Results/*/ ; do
+    processDirectory $d
+done
+
+touch output/.nojekyll
+
+./website/generate.js
---
+++ website/generate.js
@@ -0,0 +1,81 @@
+#!/usr/bin/env node
+
+const Handlebars = require('handlebars')
+const fs  = require("fs")
+const path  = require("path")
+const { lstatSync, readdirSync } = require('fs')
+const { join } = require('path')
+const util = require('util')
+
+// obj = {
+// 	"systems": [{
+// 		"name": "Mathematica_10",
+// 		"allTestsUrl": "/Results/Mathematica_10/allTests.html",
+// 		"tests": [{
+// 			"name": "And",
+// 			"url": "/Results/Mathematica_10/And_Test.html",
+// 			"tapUrl": "/Results/Mathematica_10/And_Test.tap",
+// 			"srcUrl": "/Tests/And_Test.m"
+// 		},
+// 		{
+// 			"name": "Which",
+// 			"url": "/Results/Mathematica_10/Which_Test.html",
+// 			"tapUrl": "/Results/Mathematica_10/Which_Test.tap",
+// 			"srcUrl": "/Tests/Which_Test.m"
+// 		}
+
+// 		]
+// 	}]
+// }
+
+const isDirectory = source => lstatSync(source).isDirectory()
+const getDirectories = source => readdirSync(source).map(name => join(source, name)).filter(isDirectory)
+const getHtmls = source => readdirSync(source).map(name => join(source, name)).filter( f => { return path.extname(f)==".html" && path.basename(f)!="ALL_TESTS.html" } )
+
+function getAllTests() {
+	const rootPath = path.join(__dirname,"..","output");
+	function getTest(p) {
+		const b = path.basename(p);
+		const bNoExt = path.basename(p, ".html");
+		const tapFile = path.join(path.dirname(p), bNoExt+".tap");
+		const jsonFile = tapFile + ".json";
+		const jsonObj = JSON.parse(fs.readFileSync(jsonFile, 'utf8'));
+
+		return {
+			"failed":jsonObj.stats.failures>0,
+			"stats":jsonObj.stats,
+			"url":"/"+path.relative(rootPath,p),
+			"name":b.replace("_Tests.html",""),
+			"srcUrl": "/"+path.relative(rootPath,path.join(rootPath, "Tests", bNoExt+".m")),
+			"tapUrl":"/"+path.relative(rootPath,tapFile)
+		}
+	}
+	function getSystem(p) {
+		return {
+			"name":path.basename(p).replace("_", " "),
+			"allTestsUrl":"/"+path.relative(rootPath, join(p, "ALL_TESTS.html")),
+			"tests": getHtmls(p).map(getTest)
+		}
+	}
+	function getAllSystems(p) {
+		return getDirectories(p).map(getSystem)
+	}
+	const systems = getAllSystems(path.join(__dirname,"..","output", "Results"));
+	return {
+		"systems":systems
+	}
+}
+
+function writeIndex(obj) {
+	const templateText = fs.readFileSync(path.join(__dirname, "index.html")).toString()
+	const template = Handlebars.compile(templateText);
+	const result = template(obj);
+	fs.writeFileSync(path.join(__dirname,"..","output", "index.html"), result);
+}
+
+writeIndex(getAllTests());
+
+console.log("index.html generated");
+// const obj = getAllTests();
+
+// console.log(util.inspect(obj, false, null))
