b6aba956a16e4d50c61b2048c1bc53d737ba592c
Sergey Klimov <sergey.v.klimov@gmail.com>
2018-06-03 01:08:00 -0700

    spinner

--- generate_website.sh
+++ generate_website.sh
@@ -16,12 +16,28 @@ processFile () {
 
 }
 
+spinner()
+{
+    local pid=$!
+    local delay=0.75
+    local spinstr='|/-\'
+    while [ "$(ps a | awk '{print $1}' | grep $pid)" ]; do
+        local temp=${spinstr#?}
+        printf " [%c]  " "$spinstr"
+        local spinstr=$temp${spinstr%"$temp"}
+        sleep $delay
+        printf "\b\b\b\b\b\b"
+    done
+    printf "    \b\b\b\b"
+}
+
 processDirectory () {
 	DIR=$1
 	SYSTEM_NAME=`basename $DIR`
 	echo "> $SYSTEM_NAME"
 	pushd $DIR
-	$concatTaps *.tap | node --max-old-space-size=$MAX_MEM $tapMochawesome > _ALL_TESTS_CONCAT.json
+	( $concatTaps *.tap | node --max-old-space-size=$MAX_MEM $tapMochawesome > _ALL_TESTS_CONCAT.json ) &
+	spinner
 	node --max-old-space-size=$MAX_MEM $marge -t $SYSTEM_NAME -p $SYSTEM_NAME  --reportDir ./ --showSkipped false --showHooks never --showPending false --showPassed false _ALL_TESTS_CONCAT.json
 
 	for f in *.tap ; do
